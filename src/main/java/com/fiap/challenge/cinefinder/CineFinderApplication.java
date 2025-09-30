47974b83f51d554e27fec9e947a3bfa89b0346e1
ehojonv <joaovinicius.alvesaraujo@gmail.com>
2025-09-30 10:42:10 -0300

    fix #1 - criando projeto springboot

---
+++ src/main/java/com/fiap/challenge/cinefinder/CineFinderApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.challenge.cinefinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineFinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiap/challenge/cinefinder/CineFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.challenge.cinefinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
