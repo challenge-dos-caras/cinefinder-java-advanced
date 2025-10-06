4b1448a23fb3f9f15b938a27c1d0176369d7d84d
ehojonv <joaovinicius.alvesaraujo@gmail.com>
2025-10-06 10:21:38 -0300

    update nos repositorios

--- src/main/java/com/fiap/challenge/cinefinder/repositories/GenreRepo.java
+++ src/main/java/com/fiap/challenge/cinefinder/repositories/GenreRepo.java
@@ -2,9 +2,11 @@ package com.fiap.challenge.cinefinder.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
 
 import com.fiap.challenge.cinefinder.models.Genre;
 
+@Repository
 public interface GenreRepo extends JpaRepository<Genre, Long>, JpaSpecificationExecutor<Genre> {
     
 }
--- src/main/java/com/fiap/challenge/cinefinder/repositories/ReviewRepo.java
+++ src/main/java/com/fiap/challenge/cinefinder/repositories/ReviewRepo.java
@@ -2,9 +2,11 @@ package com.fiap.challenge.cinefinder.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
 
 import com.fiap.challenge.cinefinder.models.Review;
 
+@Repository
 public interface ReviewRepo extends JpaRepository<Review, Long>, JpaSpecificationExecutor<Review> {
     
 }
